c1b9a64bfa890d8b13d5054bf1f83aa75744d66c
kellymorin <kmorin06@gmail.com>
2019-01-17 09:38:58 -0600

    Completed sales agent queries

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,1 +1,9 @@
 -- 19. top_2009_agent.sql: Which sales agent made the most in sales in 2009?
+SELECT ROUND(SUM(Invoice.Total), 2) as "Total Sales", Employee.FirstName, Employee.LastName
+FROM Invoice
+LEFT JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+LEFT JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+WHERE Invoice.InvoiceDate LIKE "%2009%"
+GROUP BY Employee.FirstName
+ORDER BY "Total Sales" DESC
+LIMIT 1
