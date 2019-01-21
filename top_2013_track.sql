fac6932dc1551a8830cf21b1cbc621342ca54b3c
kellymorin <kmorin06@gmail.com>
2019-01-21 07:57:34 -0600

    Updated individual file to reflect changes in master file

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,1 +1,8 @@
 -- 24. top_2013_track.sql: Provide a query that shows the most purchased track of 2013.
+SELECT COUNT(InvoiceLine.TrackId) as "Number of purchased tracks", Track.TrackId, Track.Name
+FROM InvoiceLine
+LEFT JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+LEFT JOIN Track ON Track.TrackId = InvoiceLine.TrackId
+WHERE Invoice.InvoiceDate LIKE "%2013%"
+GROUP BY InvoiceLine.TrackId
+ORDER BY "Number of purchased tracks" DESC
